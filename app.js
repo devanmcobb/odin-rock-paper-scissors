344f677593d90a8fb86a211ae2b89bb8231b3571
Devan Cobb <devancobb.dev@gmail.com>
2022-01-12 15:04:31 -0700

    modified app.js comments

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const resultDisplay = document.createElement('h1')
 const gameGrid = document.getElementById('game')
 gameGrid.append(playerSelectionDisplay, computerSelectionDisplay, resultDisplay)
 
-//Create choices to choose from
+//Create choices: playerSelection & computerChoice
 const choices = ['rock', 'paper', 'scissors']
 let playerSelection
 let computerChoice
 
-
-//Create playerSelection & display it
+//How does playerSelection & computerChoice work
 const handleClick = (e) => {
     playerSelection = e.target.id
     playerSelectionDisplay.innerHTML = 'Player Selection: ' + playerSelection
@@ -22,10 +21,10 @@ const handleClick = (e) => {
 const computerSelection = () => {
     const randomChoice = choices[Math.floor(Math.random() * choices.length)]
     computerChoice = randomChoice
-    computerSelectionDisplay.innerHTML = 'Computer Selection: ' + randomChoice
+    computerSelectionDisplay.innerHTML = 'Computer Selection: ' + computerChoice
 }
 
-//Create buttons
+//Create input buttons for playerSelection
 for (let i = 0; i < choices.length; i++) {
     const button = document.createElement('button')
     button.id = choices[i]
