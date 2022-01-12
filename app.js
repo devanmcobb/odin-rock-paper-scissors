93c6fe5da7163f4a524a3b493a5392583c5c8848
Devan Cobb <devancobb.dev@gmail.com>
2022-01-12 11:34:15 -0700

    more modification to app.js added getResult function for the game. next step will be to loop so there will be five rounds per game
    git status
    git status
    
    '
    ;;''[

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ const choices = ['rock', 'paper', 'scissors']
 let playerSelection
 let computerChoice
 
+
 //Create playerSelection & display it
 const handleClick = (e) => {
     playerSelection = e.target.id
     playerSelectionDisplay.innerHTML = 'Player Selection: ' + playerSelection
     computerSelection()
+    getResult()
 }
 
 const computerSelection = () => {
@@ -30,4 +32,25 @@ for (let i = 0; i < choices.length; i++) {
     button.innerHTML = choices[i]
     button.addEventListener('click', handleClick)
     gameGrid.appendChild(button)
+}
+
+//Declare results for winner
+const getResult = () => {
+    switch (playerSelection + computerChoice) {
+        case 'scissorspaper':
+        case 'rockscissors':
+        case 'paperrock':
+            resultDisplay.innerHTML = "YOU'VE WON!"
+            break
+        case 'paperscissors':
+        case 'scissorsrock':
+        case 'rockpaper':
+            resultDisplay.innerHTML = "YOU'VE LOST!"
+            break
+        case 'paperpaper':
+        case 'scissorsscissors':
+        case 'rockrock':
+            resultDisplay.innerHTML = "IT'S A DRAW!"
+            break
+    }
 }
